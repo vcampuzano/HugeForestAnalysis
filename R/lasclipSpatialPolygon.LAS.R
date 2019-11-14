052c3edae9b3de0fd0762ecd74f29a0109049a3d
Víctor M. Campuzano <victorcpkc@gmail.com>
2019-11-13 20:45:09 -0600

    Comenarios

--- R/lasclipSpatialPolygon.LAS.R
+++ R/lasclipSpatialPolygon.LAS.R
@@ -2,7 +2,10 @@ lasclipSpatialPolygon.LAS <-
 function(las, sp, projection){
   range<-rgeos::bbox2SP(bbox=bbox(las), proj4string=projection)
   ss <- rgeos::gIntersection(sp, range, drop_lower_td=TRUE)
-  if(is.null(ss)) return(NULL)
+  if(is.null(ss)){
+    print("ss es NULL")
+    return(NULL)
+  }
   las<-lidR::lasclip(las, ss)
   return(las)
 }
--- R/lasclipSpatialPolygon.LAScluster.R
+++ R/lasclipSpatialPolygon.LAScluster.R
@@ -3,7 +3,9 @@ function(las, sp, projection){
   las<-readLAS(las)
   if(is.empty(las)) return(NULL)
   las<-lasclipSpatialPolygon.LAS(las, sp, projection=projection)
-  if(!is.null(las)) 
+  if(!is.null(las))
     las <- lasfilter(las, buffer == 0)
+  else 
+    print("las es NULL")
   return(las)
 }
