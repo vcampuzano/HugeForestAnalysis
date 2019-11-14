18bb632ea57c856bd85c17c2d06a81c357a726e9
Víctor M. Campuzano <victorcpkc@gmail.com>
2019-11-14 10:10:35 -0600

    pdf param

--- R/hf.runAnalysis.R
+++ R/hf.runAnalysis.R
@@ -1,9 +1,9 @@
 hf.runAnalysis <-
-function(path=".", threads=2, projcrs=CRS("+init=epsg:32616"), showMsg=TRUE){
+function(path=".", threads=2, projcrs=CRS("+init=epsg:32616"), pdfMap=TRUE, pdfHist=TRUE, showMsg=TRUE){
   set_lidr_threads(threads)
   sectionList<-list.dirs(path, full.names=FALSE)
   for(section in sectionList[2:length(sectionList)]){
-    hf.terrainModel(section, projcrs, path=path, showMsg=showMsg)
-    hf.canopyHeightModel(section, projcrs, path=path, showMsg=showMsg)
+    hf.terrainModel(section, projcrs, path=path, pdfMap=pdfMap, showMsg=showMsg)
+    hf.canopyHeightModel(section, projcrs, path=path, pdfHist=pdfHist, showMsg=showMsg)
   }
 }
