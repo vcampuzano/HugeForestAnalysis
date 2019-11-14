b82e7cf6b434c6d1ff51434b37d46c1018986165
Víctor M. Campuzano <victorcpkc@gmail.com>
2019-11-13 20:32:29 -0600

    An error occurred when processing the chunk 10. Try to load this chunk with:  chunk <- readRDS("/tmp/RtmpFDiKfi/chunk10.rds")  las <- readLAS(chunk) Argument is not a LAS objec

--- R/lasclipSpatialPolygon.LAScluster.R
+++ R/lasclipSpatialPolygon.LAScluster.R
@@ -3,6 +3,7 @@ function(las, sp, projection){
   las<-readLAS(las)
   if(is.empty(las)) return(NULL)
   las<-lasclipSpatialPolygon.LAS(las, sp, projection=projection)
-  las <- lasfilter(las, buffer == 0)
+  if(!is.null(las)) 
+    las <- lasfilter(las, buffer == 0)
   return(las)
 }
