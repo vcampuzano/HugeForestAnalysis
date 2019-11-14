14390fcd4b632e1547c4bf888b4fb32a25899e49
Víctor M. Campuzano <victorcpkc@gmail.com>
2019-11-13 20:28:03 -0600

    Geometry type NULL not supported

--- R/lasclipSpatialPolygon.LAS.R
+++ R/lasclipSpatialPolygon.LAS.R
@@ -2,6 +2,7 @@ lasclipSpatialPolygon.LAS <-
 function(las, sp, projection){
   range<-rgeos::bbox2SP(bbox=bbox(las), proj4string=projection)
   ss <- rgeos::gIntersection(sp, range, drop_lower_td=TRUE)
+  if(is.null(ss)) return(NULL)
   las<-lidR::lasclip(las, ss)
   return(las)
 }
