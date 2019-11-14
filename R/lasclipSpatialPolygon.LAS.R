7518b6c9a75d11a8129e0803d3b83c22b3eb478b
Víctor M. Campuzano <victorcpkc@gmail.com>
2019-11-13 20:21:32 -0600

    Error: no se pudo encontrar la función "bbox2SP"

--- R/lasclipSpatialPolygon.LAS.R
+++ R/lasclipSpatialPolygon.LAS.R
@@ -1,6 +1,6 @@
 lasclipSpatialPolygon.LAS <-
 function(las, sp, projection){
-  range<-bbox2SP(bbox=bbox(las), proj4string=projection)
+  range<-rgeos::bbox2SP(bbox=bbox(las), proj4string=projection)
   ss <- gIntersection(sp, range, drop_lower_td=TRUE)
   las<-lasclip(las, ss)
   return(las)
