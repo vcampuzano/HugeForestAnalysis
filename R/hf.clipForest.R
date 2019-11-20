518c2ee6564fbdead7cca5edae384eb0adecc812
Víctor M. Campuzano <victorcpkc@gmail.com>
2019-11-20 12:43:51 -0600

    Mejorando el rendimiento

--- R/hf.clipForest.R
+++ R/hf.clipForest.R
@@ -5,8 +5,8 @@ function(section, projcrs, forest.sp, path=".", cleanSrc=FALSE, showMsg=FALSE){
   ctg=readLAScatalog(paste(fld, "nrm", sep="/"))
   opt_output_files(ctg)<-paste(fld, "forest", "{ORIGINALFILENAME}", sep="/")
   projection(ctg)<-projcrs
-  nCores=as.integer(availableCores())
-  if(showMsg) message(paste("  Multisession en", nCores, "nucleos"))
+  nCores=as.integer(availableCores()/get_lidr_threads())
+  if(showMsg) message(paste("  Multisession en", nCores, "nucleos con", get_lidr_threads(), "hilos"))
   plan(multisession, workers = nCores)
   lasclipSpatialPolygon(ctg, forest.sp, projcrs)
   plan(sequential)
