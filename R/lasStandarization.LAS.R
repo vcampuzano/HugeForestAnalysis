03dd4b9fbc09afc9aa4b8e4e30d95e610ba54f2c
Víctor M. Campuzano <victorcpkc@gmail.com>
2019-11-20 23:12:36 -0600

    Otro intento

--- R/lasStandarization.LAS.R
+++ R/lasStandarization.LAS.R
@@ -1,10 +1,10 @@
 lasStandarization.LAS <-
-function(las, density=0.5, denoise=TRUE, reclassify=c(), ...){
+function(las, density=20, denoise=TRUE, reclassify=c(), ...){
   if(denoise){
     las <- lasDenoising.LAS(las, ...)
     gc();
   }
-  las <- lasfilterdecimate(las, homogenize(density, use_pulse=TRUE))#,res=?))
+  las <- lasfilterdecimate(las, homogenize(density, use_pulse=TRUE,res=0.5))
   if(length(reclassify)>0){
     reclassify <- matrix(reclassify, ncol=2)
     for(n in 1:nrow(reclassify)) 
